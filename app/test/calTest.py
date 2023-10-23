5343bca337eeec5e3dace754b2b63db486a5f585
JasonNerd <duan912rainbow@qq.com>
2023-10-23 21:42:43 +0800

    写了个简易的python包编写教程

---
+++ app/test/calTest.py
@@ -0,0 +1,8 @@
+# file_name:      calTest.py
+# create_time:    2023/10/23-21:37
+
+from calculator.cal import *
+
+print(mul(add(3, 7), sub(5, 2)))
+
+
---
+++ app/test/calculator/__init__.py
@@ -0,0 +1,3 @@
+# file_name:      __init__.py.py
+# create_time:    2023/10/23-21:34
+

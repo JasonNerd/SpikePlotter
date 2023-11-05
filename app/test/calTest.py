64184c10d502dcbbf27e3f73687e1a124d285d6a
JasonNerd <duan912rainbow@qq.com>
2023-11-05 14:47:04 +0800

    update

--- app/test/calTest.py
+++
@@ -1,8 +0,0 @@
-# file_name:      calTest.py
-# create_time:    2023/10/23-21:37
-
-from calculator.cal import *
-
-print(mul(add(3, 7), sub(5, 2)))
-
-
--- app/test/calculator/__init__.py
+++
@@ -1,3 +0,0 @@
-# file_name:      __init__.py.py
-# create_time:    2023/10/23-21:34
-
